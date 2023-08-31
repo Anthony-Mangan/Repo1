5a03a8f70197463f2c92e441167c126eef0401e3
Anthony-Mangan <143755768+Anthony-Mangan@users.noreply.github.com>
2023-08-31 17:23:55 -0400

    Add files via upload

---
+++ CMakeProject8-29.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject8-29.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject8-29.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "My name is Anthony Mangan." << endl;
+	return 0;
+}
